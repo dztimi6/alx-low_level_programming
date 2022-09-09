c0b591a9d5bfdcff1d8ff990362837a336fb8a81
Oluwatimilehin  Adeniran <timitom60@gmail.com>
2022-09-08 17:41:49 -0700

    C program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Print out the last part of a quote in the standard error.
  *
- * Return 1 if success.
+ * Return 1 if success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-09\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-09\n", 61);
 	return (1);
 }
