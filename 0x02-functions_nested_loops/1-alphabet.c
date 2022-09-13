82319a6fbf37c41f459f61e970c296d0e62b5951
Oluwatimilehin  Adeniran <timitom60@gmail.com>
2022-09-13 08:43:33 -0700

    Loops and functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
