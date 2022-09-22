a817eb5317e6ed2c5677368348dd1e004efc59aa
Oluwatimilehin  Adeniran <timitom60@gmail.com>
2022-09-21 17:26:42 -0700

    arrays and pointers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,23 +9,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 
 /**
-
  * add_strings - Adds the numbers stored in two strings.
-
  * @n1: The string containing the first number to be added.
-
  * @n2: The string containing the second number to be added.
-
  * @r: The buffer to store the result.
-
  * @r_index: The current index of the buffer.
-
  *
-
  * Return: If r can store the sum - a pointer to the result.
-
  *         If r cannot store the sum - 0.
-
  */
 
 char *add_strings(char *n1, char *n2, char *r, int r_index)
@@ -33,8 +24,6 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 
 	int num, tens = 0;
-
-
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 
@@ -103,23 +92,14 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 }
 
 /**
-
  * infinite_add - Adds two numbers.
-
  * @n1: The first number to be added.
-
  * @n2: The second number to be added.
-
  * @r: The buffer to store the result.
-
  * @size_r: The buffer size.
-
  *
-
  * Return: If r can store the sum - a pointer to the result.
-
  *         If r cannot store the sum - 0.
-
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -127,8 +107,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 
 	int index, n1_len = 0, n2_len = 0;
-
-
 
 	for (index = 0; *(n1 + index); index++)
 
